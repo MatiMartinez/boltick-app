cabb84a60fae142aff1e5eb76d4477fff4d98af3
Matias Martinez <matias.martinez@farmaloop.cl>
2024-05-02 20:45:02 -0400

    Map function into a Fragment

--- src/components/SearchBar/components/SearchResults/index.tsx
+++ src/components/SearchBar/components/SearchResults/index.tsx
@@ -22,28 +22,32 @@ const SearchResults: FC<Props> = ({ events, isLoading, onClose }) => {
       </Flex>
     );
 
-  return events.map(({ objectID, name, category, start_date }, i) => (
-    <Flex
-      as={Link}
-      to={`/event/${objectID}`}
-      align="center"
-      gap={4}
-      paddingInline={6}
-      paddingBlock={4}
-      borderBottomRadius={events.length === i + 1 ? '3xl' : 0}
-      _hover={{ bg: '#f3f3f3', color: 'inherit' }}
-      onClick={onClose}
-      key={objectID}
-    >
-      <Avatar size="lg" name={name} src="/paax.jpeg" />
-      <Flex flexDir="column" gap={1}>
-        <Heading size="md">{name}</Heading>
-        <Text fontSize="lg" color="#777777">
-          {category} - {dateToSpanishText(start_date)}
-        </Text>
-      </Flex>
-    </Flex>
-  ));
+  return (
+    <>
+      {events.map(({ objectID, name, category, start_date }, i) => (
+        <Flex
+          as={Link}
+          to={`/event/${objectID}`}
+          align="center"
+          gap={4}
+          paddingInline={6}
+          paddingBlock={4}
+          borderBottomRadius={events.length === i + 1 ? '3xl' : 0}
+          _hover={{ bg: '#f3f3f3', color: 'inherit' }}
+          onClick={onClose}
+          key={objectID}
+        >
+          <Avatar size="lg" name={name} src="/paax.jpeg" />
+          <Flex flexDir="column" gap={1}>
+            <Heading size="md">{name}</Heading>
+            <Text fontSize="lg" color="#777777">
+              {category} - {dateToSpanishText(start_date)}
+            </Text>
+          </Flex>
+        </Flex>
+      ))}
+    </>
+  );
 };
 
 export default SearchResults;
